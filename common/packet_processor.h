ddb814fc1dd15d1ad3c7b14b067520dbcdfcbdee
Shihabur Rahman Chowdhury <sr2chowdhury@uwaterloo.ca>
2017-01-12 14:19:59 -0500

    Fix method signatures in packet_processor.h

--- common/packet_processor.h
+++ common/packet_processor.h
@@ -7,12 +7,13 @@ namespace micronf {
   template <class IngressPortType, class EgressPortType>
   class PacketProcessor {
     public:
-      void Init() = 0;
-      void Run() = 0;
-      void FlushState() = 0;
-      void RecoverState() = 0;
+      virtual void Init() = 0;
+      virtual void Run() = 0;
+      virtual void FlushState() = 0;
+      virtual void RecoverState() = 0;
       const std::string& instance_id() const { return instance_id_; }
-    private:
+      virtual ~PacketProcessor();
+    protected:
       std::string instance_id_;
       std::vector<std::unique_ptr<IngressPortType> > ingress_ports_;
       std::vector<std::unique_ptr<EgressPortType> > egress_ports_;
