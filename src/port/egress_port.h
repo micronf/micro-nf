67d0c795217d92ab54a2583e9cb9b1db4dd68600
Shihab <shihab.buet@gmail.com>
2017-02-23 12:12:21 -0500

    Modify signature of RxBurst and TxBurst functions to use C++ STL containers.

--- src/port/egress_port.h
+++ src/port/egress_port.h
@@ -1,10 +1,20 @@
 #ifndef _EGRESS_PORT_H_
 #define _EGRESS_PORT_H_
 
-#include <vector>
+#include <array>
+
+#define TX_BURST_SIZE 64
 
 class EgressPort {
   public:
-    virtual int TxBurst(void** packets, int burst_size) = 0;
+    // Send a burst of packets (maximum TX_BURST_SIZE packets) out of this port.
+    // packets contains the mbuf pointers that need to be sent. Return value is
+    // the number of actual mbufs sent over this port.
+    virtual int TxBurst(std::array<struct rte_mbuf*, TX_BURST_SIZE>& packets) = 0;
+    int port_id() const { return this->port_id_; }
+  protected:
+    // An identifier assigned to this port. Identifier assignment is specific to
+    // port and not necessarily globally unique.
+    unsigned int port_id_;
 };
 #endif // _EGRESS_PORT_H_
--- src/port/ingress_port.h
+++ src/port/ingress_port.h
@@ -1,8 +1,21 @@
 #ifndef _INGRESS_PORT_H_
 #define _INGRESS_PORT_H_
 
+#include <array>
+
+#define RX_BURST_SIZE 64
+
 class IngressPort {
   public:
-    virtual void** RxBurst(int burst_size) = 0;
+    // Receive a burst of packets from the port. Maximum burst size is
+    // determined by RX_BURST_SIZE macro defined above. The function returns the
+    // number of actual packets read from the port. When the function returns,
+    // packet will be populated with mbuf pointers.
+    virtual int RxBurst(std::array<struct mbuf*, RX_BURST_SIZE>& packets) = 0;
+    int port_id() const { return this->port_id_; }
+  protected:
+    // An identifier assigned to this port. The identifier is specific to port
+    // implementation.
+    unsigned int port_id_;
 };
 #endif // _INGRESS_PORT_H_
