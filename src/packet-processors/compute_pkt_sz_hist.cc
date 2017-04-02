a9637afe3f8811ab29cb0ccff396fe43d57ab612
Shihab <shihab.buet@gmail.com>
2017-04-02 16:22:47 -0400

    Add microservice that computes packet size distribution.

--- src/packet-processors/compute_pkt_sz_hist.cc
+++ src/packet-processors/compute_pkt_sz_hist.cc
@@ -5,25 +5,24 @@
 inline void ComputePktSzHist::Init(const PacketProcessorConfig& pp_config) {
   this->num_ingress_ports_ = pp_config.num_ingress_ports();
   this->num_egress_ports_ = pp_config.num_egress_ports();
-
   assert(this->num_egress_ports_ == 1);
-
-  ConfigurePorts(pp_config);
-  for (int i = 0; i < pkt_size_bucket_.size(); ++i) pkt_size_bucket_[i] = 0;
+  PacketProcessor::ConfigurePorts(pp_config);
+  pkt_size_bucket_.resize(kNumBuckets, 0);
 }
 
 inline void ComputePktSzHist::Run() {
   rx_pkt_array_t rx_packets;
   tx_pkt_array_t tx_packets;
+  uint16_t num_rx = 0;
+  register int i = 0;
   while (true) {
     for (auto& iport : this->ingress_ports_) {
-      int num_rx = iport->RxBurst(rx_packets);
-      int i = 0;
-      for (; i < num_rx; ++i) {
+      num_rx = iport->RxBurst(rx_packets);
+      for (i = 0; i < num_rx; ++i) {
         ++pkt_size_bucket_[rx_packets[i]->pkt_len / this->kBucketSize];
         tx_packets[i] = rx_packets[i];
       }
-      this->egress_ports_[0].TxBurst(tx_packets);
+      this->egress_ports_[0]->TxBurst(tx_packets);
     }
   }
 }
--- src/packet-processors/compute_pkt_sz_hist.h
+++ src/packet-processors/compute_pkt_sz_hist.h
@@ -1,8 +1,9 @@
 #ifndef _COMPUTE_PKT_SZ_HIST_H_
 #define _COMPUTE_PKT_SZ_HIST_H_
 
-#include <array>
 #include "packet_processor.h"
+
+#include <vector>
 
 class ComputePktSzHist : public PacketProcessor {
  public:
@@ -11,15 +12,15 @@ class ComputePktSzHist : public PacketProcessor {
   void Run() override;
   void FlushState() override;
   void RecoverState() override;
-  std::array<unsigned long, kNumBuckets>& pkt_size_bucket() const {
+  const std::vector<unsigned long>& pkt_size_bucket() const {
     return pkt_size_bucket_;
   }
 
-  virtual ~ComputePktSzHist();
+  virtual ~ComputePktSzHist() { }
 
  private:
   const int kNumBuckets;
   const int kBucketSize;
-  std::array<unsigned long, kNumBuckets> pkt_size_bucket_;
+  std::vector<unsigned long> pkt_size_bucket_;
 };
 #endif  // _COMPUTE_PKT_SZ_HIST_H_
