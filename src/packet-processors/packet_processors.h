43806c26e49ca7fdd46f74fa591b5df048aefce8
Shihab <shihab.buet@gmail.com>
2017-04-12 14:12:26 -0400

    Add HeaderParser to packet_processors.h

--- src/packet-processors/packet_processors.h
+++ src/packet-processors/packet_processors.h
@@ -4,6 +4,7 @@
 #include "compute_pkt_sz_hist.h"
 #include "count_subnet_packets.h"
 #include "drop_packet.h"
+#include "header_parser.h"
 #include "mac_swapper.h"
 #include "packet_processor.h"
 #include "tcp_ip_classifier.h"
