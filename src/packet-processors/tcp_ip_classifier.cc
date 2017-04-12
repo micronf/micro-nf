46df00788baf3b6fb220f6341b375fde41bb98e1
Shihab <shihab.buet@gmail.com>
2017-04-11 21:27:18 -0400

    Modify TCPIPClassifier to take advantage of the parser header address.

--- src/packet-processors/tcp_ip_classifier.cc
+++ src/packet-processors/tcp_ip_classifier.cc
@@ -1,7 +1,9 @@
+#include "common/protocol_header_offset.h"
 #include "tcp_ip_classifier.h"
 #include <rte_ether.h>
 #include <rte_ip.h>
 #include <rte_mbuf.h>
+#include <rte_prefetch.h>
 #include <rte_tcp.h>
 
 void TCPIPClassifier::Init(const PacketProcessorConfig& pp_config) {
@@ -37,19 +39,23 @@ void TCPIPClassifier::Run() {
   tx_pkt_array_t deny_packets;
   uint16_t nb_allow = 0, nb_deny = 0, num_rx = 0;
   register int iport_idx = 0, i = 0;
+  uint64_t mdata_addr = 0;
   struct ether_hdr* ethernet = nullptr;
   struct ipv4_hdr* ipv4 = nullptr;
   struct tcp_hdr* tcp = nullptr;
+  struct ProtocolHeaderOffset* header_offsets = nullptr;
   while (true) {
     for (iport_idx = 0; iport_idx < num_ingress_ports_; ++iport_idx) {
       num_rx = ingress_ports_[iport_idx]->RxBurst(packets);
       nb_allow = 0;
       nb_deny = 0;
       for (i = 0; i < num_rx; ++i) {
-        // rte_prefetch0(rte_pktmbuf_mtod(packet, void*));
-        ethernet = rte_pktmbuf_mtod(packets[i], struct ether_hdr*);
-        ipv4 = reinterpret_cast<struct ipv4_hdr*>(ethernet + 1);
-        tcp = reinterpret_cast<struct tcp_hdr*>(ipv4 + 1);
+        header_offsets = reinterpret_cast<ProtocolHeaderOffset*>(
+            reinterpret_cast<uint64_t>(packets[i] + sizeof(rte_mbuf)));
+        rte_prefetch0(reinterpret_cast<void*>(packets[i]->buf_addr));
+        ethernet = reinterpret_cast<ether_hdr*>(header_offsets->eth_hdr_addr);
+        ipv4 = reinterpret_cast<ipv4_hdr*>(header_offsets->ip_hdr_addr);
+        tcp = reinterpret_cast<tcp_hdr*>(header_offsets->transport_hdr_addr);
         for (auto& rule : acl_rules_[iport_idx]) {
           if (rule.Match(ipv4->src_addr, ipv4->dst_addr, tcp->src_port,
                          tcp->dst_port)) {
